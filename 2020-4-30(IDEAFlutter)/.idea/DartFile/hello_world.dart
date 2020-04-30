2910b73f2ee2ff6815428757d33daeb204105928
Leon Kang <kangleonjy@gmail.com>
2020-04-30 22:15:54 +0800

    new
    new

--- 2020-4-30(IDEAFlutter)/.idea/DartFile/hello_world.dart
+++ 2020-4-30(IDEAFlutter)/.idea/DartFile/hello_world.dart
@@ -1,4 +1,20 @@
 
 void main() {
-  print("Hello world,I am dart!");
+//  print("Hello world,I am dart!");
+  String str="This is";
+  String str1="second str";
+  print(str + "New");
+  print(str * 5);
+  print(str == str1);
+  print(str[0]);
+
+  int a=1;
+  int b=2;
+  print("a+b = ${a+b}");
+
+  var list = new List<dynamic>();
+  list.add(1);
+  list.add("hello");
+  list.add(true);
+  print(list);
 }
