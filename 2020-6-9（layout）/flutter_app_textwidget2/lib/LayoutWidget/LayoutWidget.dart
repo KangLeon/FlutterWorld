51ff59625486ec998e732346c3a92e3c3e44a887
JY <tengjiaoji@blackfish.cn>
2020-06-12 23:33:36 +0800

    new
    new

--- 2020-6-9（layout）/flutter_app_textwidget2/lib/LayoutWidget/LayoutWidget.dart
+++ 2020-6-9（layout）/flutter_app_textwidget2/lib/LayoutWidget/LayoutWidget.dart
@@ -4,13 +4,65 @@ class LayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+//      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        IconBadge(Icons.pool)
+        StackWidget(),
       ],
     );
   }
 }
+
+class StackWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: <Widget>[
+        SizedBox(
+          width: 200.0,
+          height: 300.0,
+          child: Container(
+            alignment: Alignment(0.0, 0.0),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+
+          ),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        SizedBox(
+          width: 100.0,
+          height: 100.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              shape: BoxShape.circle,
+              gradient: RadialGradient(
+                colors: [
+                  Color.fromRGBO(7, 102, 255, 1.0),
+                  Color.fromRGBO(3, 54, 255, 1.0),
+                ],
+              ),
+            ),
+            child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0,),
+          ),
+        ),
+        Positioned(
+          right: 20.0,
+          top: 20.0,
+          child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0,),
+        ),
+      ],
+    );
+  }
+}
+
 class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
