5258d0ee653408c5f8345a519dd829336788eb82
Leon Kang <kangleonjy@gmail.com>
2020-10-08 15:24:15 +0800

    new
    new

--- 2020-10-8/flutter_travel/lib/pages/home_page.dart
+++ 2020-10-8/flutter_travel/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +7,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var _imageUrls = [
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3362413416,132145148&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3909335605,2762532975&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2722246159,3212394634&fm=15&gp=0.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: [
+            Container(
+              height: 160,
+              child: Swiper(
+                  itemCount: 3,
+                autoplay: true,
+                itemBuilder: (BuildContext context,int index){
+                    return Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.fill,
+                    );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
