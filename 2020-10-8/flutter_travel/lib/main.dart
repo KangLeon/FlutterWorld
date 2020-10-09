af332f59b7ed05b1ae8d28e395d59a1b2108886a
Leon Kang <kangleonjy@gmail.com>
2020-10-09 22:18:23 +0800

    new
    new

--- 2020-10-8/flutter_travel/lib/main.dart
+++ 2020-10-8/flutter_travel/lib/main.dart
@@ -1,33 +1,84 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_travel/navigator/tab_navigator.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String showResult = '';
+
+  Future<CommonModel> fetchpst() async{
+    final response = await http.get('http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final result = jsonDecode(response.body);
+    return CommonModel.fromJson(result);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Future与FutureBuilfer实用技巧'),
+        ),
+        // ignore: missing_return
+        body: FutureBuilder<CommonModel>(
+            future: fetchpst(),
+            // ignore: missing_return
+            builder: (BuildContext context, AsyncSnapshot<CommonModel> snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              return Text('In put a URL to start');
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator(),);
+            case ConnectionState.active:
+              return Text('');
+            case ConnectionState.done:
+              if(snapshot.hasError){
+                return Text(
+                  '${snapshot.error}',
+                  style: TextStyle(color: Colors.red),
+                );
+              }else{
+                return Column(children: [
+                  Text('icon:${snapshot.data.icon}'),
+                  Text('statusBarColor:${snapshot.data.statusBarColor}'),
+                  Text('title:${snapshot.data.title}'),
+                  Text('url:${snapshot.data.url}'),
+                ]);
+              }
+          }
+        }),
       ),
-      home: TabNavigator(),
     );
   }
 }
+
+class CommonModel{
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel({this.icon,this.title,this.url,this.statusBarColor,this.hideAppBar});
+
+  factory CommonModel.fromJson(Map<String,dynamic>json){
+    return CommonModel(
+      icon: json['icon'],
+      title: json['title'],
+      url: json['url'],
+      statusBarColor: json['statusBarColor'],
+      hideAppBar: json['hideAppBar'],
+    );
+  }
+}
+
--- 2020-10-8/flutter_travel/lib/pages/home_page.dart
+++ 2020-10-8/flutter_travel/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
+const APPBAR_SCROLL_OFFSET = 100;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -14,38 +16,81 @@ class _HomePageState extends State<HomePage> {
     'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2722246159,3212394634&fm=15&gp=0.jpg'
   ];
 
+  double _appBarAlpha = 0;
+
+  _onScroll(offset){
+    double alpha = offset/APPBAR_SCROLL_OFFSET;
+    if(alpha<0){
+      alpha=0;
+    }else if(alpha>1){
+      alpha=1;
+    }
+
+    setState(() {
+      _appBarAlpha=alpha;
+    });
+    print(alpha);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MediaQuery.removePadding(
-        removeTop: true,
-          context: context,
-          child: ListView(
-            children: [
-              Container(
-                height: 160,
-                child: Swiper(
-                  itemCount: 3,
-                  autoplay: true,
-                  itemBuilder: (BuildContext context,int index){
-                    return Image.network(
-                      _imageUrls[index],
-                      fit: BoxFit.fill,
-                    );
-                  },
-                  pagination: SwiperPagination(),
-                ),
-              ),
-              Container(
-                height: 800,
-                child: ListTile(
-                  title: Text(
-                    '哈哈',
-                  ),
+      body: Stack(
+        children: [
+          MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: NotificationListener(
+                // ignore: missing_return
+                onNotification: (scrollNotification){
+                  if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth==0){
+                    _onScroll(scrollNotification.metrics.pixels);
+                  }
+                },
+                child: ListView(
+                  children: [
+                    Container(
+                      height: 160,
+                      child: Swiper(
+                        itemCount: 3,
+                        autoplay: true,
+                        itemBuilder: (BuildContext context,int index){
+                          return Image.network(
+                            _imageUrls[index],
+                            fit: BoxFit.fill,
+                          );
+                        },
+                        pagination: SwiperPagination(),
+                      ),
+                    ),
+                    Container(
+                      height: 800,
+                      child: ListTile(
+                        title: Text(
+                          '哈哈',
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               )
-            ],
           ),
+          Opacity(
+              opacity: _appBarAlpha,
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.white
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text('首页'),
+                  ),
+                ),
+              ),
+          ),
+        ],
       ),
     );
   }
