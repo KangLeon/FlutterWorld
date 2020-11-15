4508da3772b931cade1bcb2d226ec7dd462239d3
Leon Kang <kangleonjy@gmail.com>
2020-11-15 16:28:54 +0800

    new
    new

---
+++ 2020-11-11/flutter_app_test/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<tencent_im_plugin/TencentImPlugin.h>)
+#import <tencent_im_plugin/TencentImPlugin.h>
+#else
+@import tencent_im_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [TencentImPlugin registerWithRegistrar:[registry registrarForPlugin:@"TencentImPlugin"]];
+}
+
+@end
