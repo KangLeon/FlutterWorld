faabe4e9d181100ed819b857e767ffea1345b1d1
Leon Kang <kangleonjy@gmail.com>
2021-02-28 21:07:44 +0800

    new
    new

---
+++ 2020-12-31/flutter_app_webview/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  WebViewController _controller;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return SafeArea(
+        child: Stack(
+            children: <Widget>[
+              WebView(
+                userAgent: "flutter",
+                initialUrl: "https://rzp.io/i/ilpU1Si5l",
+                //JS执行模式 是否允许JS执行
+                javascriptMode: JavascriptMode.unrestricted,
+
+                onWebViewCreated: (controller) {
+                  _controller = controller;
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  print('重定向的网址============: ${request.url}');
+                  return NavigationDecision.navigate;
+                },
+                onPageFinished: (url) {
+                  print('页面加载结束============: $url');
+                },
+                onPageStarted: (String url) {
+                  print('页面开始加载============: $url');
+                },
+                onWebResourceError: (error){
+                  print("页面资源错误==========: $error");
+                },
+                javascriptChannels: [
+                  JavascriptChannel(
+                      name: 'close',//handleName
+                      onMessageReceived: (JavascriptMessage message) {
+                        print(message.message);
+                        //接收到js返回的数据
+                        print("get message from JS, message is: ${message.message}");
+
+                      }),
+                ].toSet(),
+              ),
+              // Offstage(
+              //   offstage: !isLoading,
+              //   child: Center(
+              //     child: CupertinoActivityIndicator(
+              //         animating: true,
+              //         radius: 25
+              //     ),
+              //   ),
+              // )
+            ]
+        )
+    );
+  }
+}
