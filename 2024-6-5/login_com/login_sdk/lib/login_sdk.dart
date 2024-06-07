6836d6a6c3f0fc729664b5dd5c76643a86c6a7a4
JY <397879704@qq.com>
2024-06-07 16:02:18 +0800

    组件化开发

---
+++ 2024-6-5/login_com/login_sdk/lib/login_sdk.dart
@@ -0,0 +1,13 @@
+/*
+ * @Author: JY 397879704@qq.com
+ * @Date: 2024-06-07 15:47:13
+ * @LastEditors: JY 397879704@qq.com
+ * @LastEditTime: 2024-06-07 16:00:53
+ * @FilePath: /login_sdk/lib/login_sdk.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+library login_sdk;
+
+class LoginConfig {
+  int addOne(int value) => value + 1;
+}
